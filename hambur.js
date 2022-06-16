13fab85d4059f479017644e16be5f36cdc55db72
cathy <cathy@bubulazi.com>
2022-06-16 14:03:46 +0100

    Proyecto hambuerguesa terminado

--- hambur.js
+++ hambur.js
@@ -1,1 +1,17 @@
-const nav = document.querySelector('')
+const btn = document.querySelector('#btn')
+const nav = document.querySelector('#navegacion')
+
+btn.addEventListener('click', abrirHamburguesa)
+
+function abrirHamburguesa() {
+  const visibilidad = nav.getAttribute('data-visible')
+  if(visibilidad === 'false') {
+    nav.setAttribute('data-visible', true)
+    btn.setAttribute('aria-expanded', true)
+  } else {
+    nav.setAttribute('data-visible', false)
+    btn.setAttribute('aria-expanded', false)
+    
+
+  }
+}
